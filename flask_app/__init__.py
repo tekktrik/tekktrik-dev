3c72638e7461861bcf1a1a54dc879d9dc28e04bf
Alec Delaney <tekktrik@gmail.com>
2023-12-29 14:41:05 -0500

    Updated per pre-commit

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,18 @@
 # SPDX-FileCopyrightText: 2023 Alec Delaney
 # SPDX-License-Identifier: MIT
 
+"""
+Main entry point for the flask application
+
+Author: Alec Delaney
+"""
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    """Route for index (landing page)"""
     return "<p>Hello, world!</p>"
