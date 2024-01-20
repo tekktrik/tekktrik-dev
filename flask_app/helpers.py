681d99251376251ab6013911e10c1e4784283d6a
Alec Delaney <tekktrik@gmail.com>
2024-01-20 00:05:32 -0500

    Add jobs to About page

--- flask_app/helpers.py
+++ flask_app/helpers.py
@@ -7,10 +7,25 @@ Helper functions used by the Flask application
 Author: Alec Delaney
 """
 
+import datetime
 import json
-from typing import Any
+from typing import Any, TypedDict
 
+import dateutil.tz
 import requests
+
+
+class JobDict(TypedDict):
+    """TypedDict representing job JSON file contents"""
+
+    title: str
+    employer: str
+    location: str
+    startDate: str
+    endDate: str | None
+    time: str
+    duties: list[str]
+    skills: list[str]
 
 
 def generate_settings_json(zipcode: str) -> str:
@@ -37,3 +52,52 @@ def get_repo_info(token: str) -> tuple[dict[str, Any], dict[str, Any]]:
     json_resp = json.loads(resp.content)["data"]["user"]
 
     return json_resp["contributionsCollection"], json_resp["repositories"]
+
+
+def sort_jobs_start_date(job: JobDict) -> int:
+    """Sort the jobs by start date"""
+    month_str, year_str = job["startDate"].split("/")
+    return int(year_str) * 100 + int(month_str)
+
+
+def consolidate_sorted_jobs(jobs: list[JobDict]) -> list[list[JobDict]]:
+    """Consolidate jobs in instances like promotions"""
+    grouped_jobs_dict: dict[str, list[JobDict]] = {}
+    grouped_jobs_list: list[list[JobDict]] = []
+
+    for job in jobs:
+        newly_added = False
+        employer = job["employer"]
+
+        # If not already in dict, add
+        if employer not in grouped_jobs_dict:
+            grouped_jobs_dict[employer] = [job]
+            newly_added = True
+
+        # Get different of start and end of roles
+        start_role = datetime.datetime.strptime(
+            grouped_jobs_dict[employer][-1]["startDate"], "%m/%Y"
+        )
+        if job["endDate"] == "current":
+            end_role = datetime.datetime.now(dateutil.tz.UTC)
+        else:
+            end_role = datetime.datetime.strptime(job["endDate"], "%m/%Y")
+
+        # If job was not just newly added and gap is no more than 31 days apart
+        # then add to existing list
+        if not newly_added and (start_role - end_role).days <= 31:
+            grouped_jobs_dict[employer].append(job)
+
+        elif not newly_added:
+            grouped_jobs_list.append(grouped_jobs_dict[employer])
+            grouped_jobs_dict[employer] = [job]
+
+    for remaining_job in grouped_jobs_dict.values():
+        grouped_jobs_list.append(remaining_job)
+
+    return grouped_jobs_list
+
+
+def sort_grouped_jobs(jobs_list: list[JobDict]) -> int:
+    """Sort the grouped lists of jobs"""
+    return sort_jobs_start_date(jobs_list[0])
